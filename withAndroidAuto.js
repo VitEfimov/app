8e7401275ff132a5d9316056cf8ea413f17ee5a5
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2026-08-25 22:06:27 -0400

    fix error

--- withAndroidAuto.js
+++ withAndroidAuto.js
@@ -1,6 +1,34 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest, withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const automotiveAppDescXml = `<?xml version="1.0" encoding="utf-8"?>
+<automotiveApp>
+    <uses name="notification"/>
+</automotiveApp>
+`;
 
 module.exports = function withAndroidAuto(config) {
+  config = withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const xmlDir = path.join(
+        config.modRequest.platformProjectRoot,
+        'app',
+        'src',
+        'main',
+        'res',
+        'xml'
+      );
+      if (!fs.existsSync(xmlDir)) {
+        fs.mkdirSync(xmlDir, { recursive: true });
+      }
+      const xmlPath = path.join(xmlDir, 'automotive_app_desc.xml');
+      fs.writeFileSync(xmlPath, automotiveAppDescXml, 'utf8');
+      return config;
+    },
+  ]);
+
   return withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults;
     const application = androidManifest.manifest.application?.[0];
